c4d85990db1201baed77996dbf097f8902f3bb40
alexjones <ajonzy@gmail.com>
2021-03-19 10:35:36 -0600

    Added the get books by user endpoint

--- app.py
+++ app.py
@@ -121,6 +121,11 @@ def get_all_books():
     all_books = db.session.query(Book).all()
     return jsonify(multiple_book_schema.dump(all_books))
 
+@app.route("/book/get/<user_id>", methods=["GET"])
+def get_all_books_by_user(user_id):
+    all_books = db.session.query(Book).filter(Book.user_id == user_id).all()
+    return jsonify(multiple_book_schema.dump(all_books))
+
 
 if __name__ == "__main__":
     app.run(debug = True)
