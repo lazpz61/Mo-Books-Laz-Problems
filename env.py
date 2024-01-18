d2cdf4b40ccb5dc3dfb840ced0786ecfbd57283a
Lazaro Perez <lazpz61@gmail.com>
2024-01-18 00:00:16 -0500

    updating app.py

--- env.py
+++ env.py
@@ -2,6 +2,4 @@
 
 import os
 
-os.environ["SQLALCHEMY_DATABASE_URI"] = "postgres://tfibcscioehwsk:5086e084578f92ef1dd3a660485bc8b877e95495cec7e3cbf8af2746e30bb7a2@ec2-54-234-13-16.compute-1.amazonaws.com:5432/d11n71ll3jjsu5"
-
-DATABASE_URL= "postgres://tfibcscioehwsk:5086e084578f92ef1dd3a660485bc8b877e95495cec7e3cbf8af2746e30bb7a2@ec2-54-234-13-16.compute-1.amazonaws.com:5432/d11n71ll3jjsu5"
+os.environ["SQLALCHEMY_DATABASE_URI"] = "postgres://mitsdpdwthqhjm:44854ba8e1aa0c98447aa9b31555e756d54e229d1bacc2a1fe6550b6b37968eb@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d20dvgpmn4ioh3"
