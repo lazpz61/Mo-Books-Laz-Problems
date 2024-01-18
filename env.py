5b7be04b74a19ce1fce39266de56f3918cbede0c
Lazaro Perez <lazpz61@gmail.com>
2024-01-17 22:09:13 -0500

    updating PostgresSQL location

--- env.py
+++ env.py
@@ -2,4 +2,4 @@
 
 import os
 
-os.environ["SQLALCHEMY_DATABASE_URI"] = "postgres://uawixqerjzcvxm:42fa8cf6bc4355ad730d9c8e897fa13d91299ab79591903dad18a65faa9331df@ec2-3-91-127-228.compute-1.amazonaws.com:5432/dao4f5kujs1tas"
+os.environ["SQLALCHEMY_DATABASE_URI"] = "postgres://tfibcscioehwsk:5086e084578f92ef1dd3a660485bc8b877e95495cec7e3cbf8af2746e30bb7a2@ec2-54-234-13-16.compute-1.amazonaws.com:5432/d11n71ll3jjsu5"
