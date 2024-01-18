99fc1433327816a930ba6a4f5271eec7ed3ebdea
Lazaro Perez <lazpz61@gmail.com>
2024-01-17 23:54:07 -0500

    Updating FIles for Deployment

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URL")
 print(os.environ.get("SQLALCHEMY_DATABASE_URI"))
 
 db = SQLAlchemy(app)
--- env.py
+++ env.py
@@ -3,3 +3,5 @@
 import os
 
 os.environ["SQLALCHEMY_DATABASE_URI"] = "postgres://tfibcscioehwsk:5086e084578f92ef1dd3a660485bc8b877e95495cec7e3cbf8af2746e30bb7a2@ec2-54-234-13-16.compute-1.amazonaws.com:5432/d11n71ll3jjsu5"
+
+DATABASE_URL= "postgres://tfibcscioehwsk:5086e084578f92ef1dd3a660485bc8b877e95495cec7e3cbf8af2746e30bb7a2@ec2-54-234-13-16.compute-1.amazonaws.com:5432/d11n71ll3jjsu5"
