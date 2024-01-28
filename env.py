5a64dc7aece2a6bc15848dbad125a03e6a64b4ab
Lazaro Perez <lazpz61@gmail.com>
2024-01-27 20:04:04 -0500

    including Database URI

--- env.py
+++ env.py
@@ -2,4 +2,4 @@
 
 import os
 
-os.environ["SQLALCHEMY_DATABASE_URI"] = "postgres://mitsdpdwthqhjm:44854ba8e1aa0c98447aa9b31555e756d54e229d1bacc2a1fe6550b6b37968eb@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d20dvgpmn4ioh3"
+os.environ["SQLALCHEMY_DATABASE_URI"] = "postgres://bberqgpolincum:eef17db2fcda2ddc2362fadedb938167d195d3dc623f5cb455245e63560aeed0@ec2-44-213-151-75.compute-1.amazonaws.com:5432/d5o4931kre5tm9"
