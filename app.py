bbf27aacd6a18a1b46e618d4f7a08c6e200b269a
Lazaro Perez <lazpz61@gmail.com>
2024-01-28 03:25:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 print(os.environ.get("SQLALCHEMY_DATABASE_URI"))
 
 db = SQLAlchemy(app)
