d666b6e3a1a130cbe9ee104ce8a53e0f0835b5fb
Lazaro Perez <lazpz61@gmail.com>
2024-01-28 13:38:42 -0500

    updating url

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_heroku import Heroku
 from flask_bcrypt import Bcrypt
-from dotenv import load_dotenv
+
 
 import os
 # import env Making change so that I can do a Git Push
-load_dotenv()
 
 
+SQLALCHEMY_DATABASE_URI = "postgres://bberqgpolincum:eef17db2fcda2ddc2362fadedb938167d195d3dc623f5cb455245e63560aeed0@ec2-44-213-151-75.compute-1.amazonaws.com:5432/d5o4931kre5tm9"
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
