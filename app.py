a4731ce2856893ee817b5210c839b3d1bce718f0
Lazaro Perez <lazpz61@gmail.com>
2024-01-28 15:38:13 -0500

    included import os

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from flask_bcrypt import Bcrypt
 
 
 import os
+import env
 # import env Making change so that I can do a Git Push
 
 
-SQLALCHEMY_DATABASE_URI = "postgres://bberqgpolincum:eef17db2fcda2ddc2362fadedb938167d195d3dc623f5cb455245e63560aeed0@ec2-44-213-151-75.compute-1.amazonaws.com:5432/d5o4931kre5tm9"
+# SQLALCHEMY_DATABASE_URI = "postgres://bberqgpolincum:eef17db2fcda2ddc2362fadedb938167d195d3dc623f5cb455245e63560aeed0@ec2-44-213-151-75.compute-1.amazonaws.com:5432/d5o4931kre5tm9"
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
