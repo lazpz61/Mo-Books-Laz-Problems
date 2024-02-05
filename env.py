484673e677e05235cc330e3ac8898a8a4b0ad039
Lazaro Perez <lazpz61@gmail.com>
2024-02-04 23:20:06 -0500

    updating databse URL

--- env.py
+++ env.py
@@ -2,4 +2,4 @@
 
 import os
 
-os.environ["SQLALCHEMY_DATABASE_URI"] = "postgresql://bberqgpolincum:eef17db2fcda2ddc2362fadedb938167d195d3dc623f5cb455245e63560aeed0@ec2-44-213-151-75.compute-1.amazonaws.com:5432/d5o4931kre5tm9"
+os.environ["SQLALCHEMY_DATABASE_URI"] = "postgres://bcpaunrklwevuo:5409d1936346fc64e54e18154baf46c7527cd57e8d5548bbf7cedaca74d4f0f2@ec2-18-205-44-21.compute-1.amazonaws.com:5432/d5ikd7bbko87qc"
