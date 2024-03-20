a4360a5f9eea5ff67c97dfd3f2e53c2f399d2cfd
Lazaro Perez <lazpz61@gmail.com>
2024-03-20 18:44:25 -0400

    Updating Database

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # Config for PostgreSQL database
 # SQLALCHEMY_DATABASE_URI = "postgres://bcpaunrklwevuo:5409d1936346fc64e54e18154baf46c7527cd57e8d5548bbf7cedaca74d4f0f2@ec2-18-205-44-21.compute-1.amazonaws.com:5432/d5ikd7bbko87qc"
-SQLALCHEMY_DATABASE_URI = "postgres://cyltzyptrplgfn:6c23b954679fd958e8ac432cf21fe52fdfe451bb07461168f776c85093d5b3f6@ec2-34-233-242-44.compute-1.amazonaws.com:5432/db3pt3teidq7l1"
+SQLALCHEMY_DATABASE_URI = "postgres://ifspbzquukhbfj:39fdb17572a9b0bcf1dfd4aa58611750954559efe96799a469600a4792bd30ef@ec2-18-208-0-191.compute-1.amazonaws.com:5432/d7frrt0ju5n59o"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # print(os.environ.get("SQLALCHEMY_DATABASE_URI"))
